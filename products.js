c4a7f391c4e9a929cf3cdce3b215a740f295d125
MateuszGruntkowski <121804153+MateuszGruntkowski@users.noreply.github.com>
2023-01-02 15:12:32 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,219 @@
+const product = [
+    {
+      id: 1,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 2,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 3,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 4,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 5,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 6,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 7,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 8,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 9,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 10,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 11,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 12,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 13,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 14,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 15,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 16,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 17,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 18,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 19,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 20,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 21,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 22,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 23,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+    {
+      id: 24,
+      name: "PS5",
+      price: 400.00,
+      instock: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./images/ps5.jpg",
+    },
+
+]
